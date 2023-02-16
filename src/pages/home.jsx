31a216c390bc6a558e04f454aa920f6748d318dd
Khawar Sultan <Khawarsultan663@gmail.com>
2023-02-16 19:55:52 +0500

    Appartment Changes commit

---
+++ src/pages/home.jsx
@@ -0,0 +1,14 @@
+import './home.css';
+import "../components/navbar.css";
+function home ()  {
+  return (
+    <div className="home-background" >
+      <h1> Modern  <span>  Appartment House</span> </h1>
+      <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam tempora, ex unde, quidem ut ratione voluptatem inventore sequi voluptas quis blanditiis. Corrupti quisquam delectus maiores impedit beatae, expedita reiciendis nulla? </p>
+      <a href="" className="nav-link">Read More</a>
+
+    </div>
+  );
+};
+
+export default home;
